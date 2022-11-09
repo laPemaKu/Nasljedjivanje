14b05f759213f2d22b4da5f97156f6ae0cfb1fc2
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2022-11-09 09:27:10 +0100

    Add files via upload

---
+++ Nasljedjivanje/Nasljedjivanje/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljedjivanje
+{
+    class Cat
+    {
+        bool opasna;
+        public bool Opasna { get => opasna; set => opasna = value; }
+
+        public virtual void Opasnost()
+        {
+            this.opasna = false;
+        }
+        public override string ToString()
+        {
+            return "Maca - opasnost = " + this.Opasna;
+        }
+    }
+    class Gepard : Cat
+    {
+        public override void Opasnost()
+        {
+            this.Opasna= true;
+        }
+        public override string ToString()
+        {
+            return "Gepard - opasnost = " + this.Opasna;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gepard g= new Gepard();
+            g.Opasnost();
+            Console.WriteLine(g.ToString());
+
+            Cat c= new Cat();
+            c.Opasnost();
+            Console.WriteLine(c.ToString());
+            Console.ReadKey();
+        }
+    }
+   
+}
